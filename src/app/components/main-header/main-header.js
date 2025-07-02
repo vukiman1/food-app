b9e95539dd926c4e70c97edbcc1af7871887d091
Kim An <anvu734@gmail.com>
2025-07-02 23:46:48 +0700

    chore: deletefile package-lock.json, project.mdc, install better-sqlite3

--- src/app/components/main-header/main-header.js
+++ src/app/components/main-header/main-header.js
@@ -3,6 +3,7 @@ import classes from "./main-header.module.css";
 import logoImage from "@/assets/logo.png";
 import Image from "next/image";
 import MainHeaderBackground from "./main-header-background";
+import NavLink from "./nav-link";
 const MainHeader = () => {
   return (
     <>
@@ -15,13 +16,13 @@ const MainHeader = () => {
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link href="/meals">Meals</Link>
+              <NavLink href="/meals">Meals</NavLink>
             </li>
             <li>
-              <Link href="/community">Community</Link>
+              <NavLink href="/community">Community</NavLink>
             </li>
             <li>
-              <Link href="/meals/share">Share a Meal</Link>
+              <NavLink href="/meals/share">Share a Meal</NavLink>
             </li>
           </ul>
         </nav>
--- src/app/meals/page.js
+++ src/app/meals/page.js
@@ -1,8 +1,24 @@
+import Link from "next/link";
+import classes from "./page.module.css";
+import MealsGrid from "../components/meals/meals-grid";
+
 const Meals = () => {
   return (
-    <div>
-      <h1>Meals</h1>
-    </div>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>Choose your favorite meal and share it with the community.</p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share Your Favorite Meal</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
   );
 };
 
